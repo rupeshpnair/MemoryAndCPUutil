1296868382d56c84ead864c996fdeb0c733d320f
Rupesh Nair <rupeshnair@pop-os.localdomain>
2023-01-22 00:16:34 +0530

    added a ling

--- MemoryAndCPUutil.sh
+++ MemoryAndCPUutil.sh
@@ -1,2 +1,3 @@
 echo "Memory utilization = $(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')"
 echo "CPU utilization = $(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')"
+echo "hello"
